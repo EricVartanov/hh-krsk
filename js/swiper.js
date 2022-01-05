22eda1408b6e6a062020cc58c3e0d92d559db550
EricVartanov <82024638+EricVartanov@users.noreply.github.com>
2022-01-05 15:50:35 +0300

    Add files via upload

---
+++ js/swiper.js
@@ -0,0 +1,40 @@
+const comfortSwiper = new Swiper('.idc-comfort__swiper .swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+
+
+    // Default parameters
+    slidesPerView: 1.6,
+    spaceBetween: 5,
+    autoplay: false,
+
+    // Responsive breakpoints
+    breakpoints: {
+        // when window width is >= 320px
+        360: {
+            slidesPerView: 2.2,
+            spaceBetween: 5
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 2.2,
+            spaceBetween: 10
+        },
+        // when window width is >= 640px
+        700: {
+            slidesPerView: 3,
+            spaceBetween: 10
+        }
+    },
+
+    pagination: {
+        el: '.idc-comfort__swiper .swiper-pagination',
+        type: 'bullets',
+    },
+
+    navigation: {
+        nextEl: '.idc-swiper-button-next',
+        prevEl: '.idc-swiper-button-prev',
+    },
+});
